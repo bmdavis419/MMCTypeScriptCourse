10565647ad2a5b706d351daae048727644592f13
Phoenix41902 <bmdavis419@protonmail.com>
2021-04-19 00:45:25 -0400

    created week 2

--- public/index.js
+++ public/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("TEST");
+// Console Log
+console.log("Hello world!");
+// vars
+var strFirstVar = "var";
+console.log(strFirstVar);
+// Types
+var strName = "Ben";
+var numAge = "19";
+var boolIsInstructor = true;
+// User Input
+// Import the prompt module
+var inputSource = require("prompt-sync")();
+strName = inputSource("Enter a new name: ");
+console.log(strName);
+// Condidtionals
+if (strName.length > 5) {
+    console.log("The name you entered is long");
+}
+else {
+    console.log("The name you entered is short");
+}
+// Embedded expressons in strings
+console.log("The name you entered was " + strName + ", it has a length of " + strName.length);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("TEST");
+// Console Log
+console.log("Hello world!");
+
+// vars
+let strFirstVar = "var";
+console.log(strFirstVar);
+
+// Types
+let strName: string = "Ben";
+let numAge = "19";
+let boolIsInstructor = true;
+
+// User Input
+// Import the prompt module
+const inputSource = require("prompt-sync")();
+strName = inputSource("Enter a new name: ");
+console.log(strName);
+
+// Condidtionals
+if (strName.length > 5) {
+	console.log("The name you entered is long");
+} else {
+	console.log("The name you entered is short");
+}
+
+// Embedded expressons in strings
+console.log(
+	`The name you entered was ${strName}, it has a length of ${strName.length}`
+);
